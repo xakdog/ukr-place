80a603805327ac06e165b188c16dce48d76f8b02
xakdog <xakdog@gmail.com>
2022-03-28 15:15:10 +0200

    Buying pixels

--- programs/ukr_place/src/lib.rs
+++ programs/ukr_place/src/lib.rs
@@ -1,15 +1,117 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+macro_rules! eligible_charities {
+    () => {
+        [
+            // Website: https://donate.thedigital.gov.ua/
+            "66pJhhESDjdeBBDdkKmxYYd7q6GUggYPWjxpMKNX39KV",
+        ]
+    };
+}
+
+const PRICE_PER_PIXEL: u64 = 25_000_000; // lamports
 
 #[program]
 pub mod ukr_place {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn buy_pixels(ctx: Context<BuyPixels>, amount: u32) -> Result<()> {
+        let pixel_wallet = &mut ctx.accounts.pixel_wallet;
+
+        if !charity::is_eligible(ctx.accounts.charity.key) {
+            return Err(error!(ErrorCode::UnknownCharityAccount));
+        }
+
+        charity::transfer(
+            &ctx.accounts.user,
+            &ctx.accounts.charity,
+            u64::from(amount) * PRICE_PER_PIXEL,
+        )?;
+
+        pixel_wallet.available_pixels += u64::from(amount);
+
+        Ok(())
+    }
+}
+
+mod charity {
+    use super::*;
+    use std::collections::HashSet;
+
+    pub fn is_eligible(key: &Pubkey) -> bool {
+        let key_str = key.to_string();
+        let eligible = HashSet::from(eligible_charities!());
+
+        eligible.contains(key_str.as_str())
+    }
+
+    pub fn transfer<'info>(
+        patron_account: &Signer<'info>,
+        fund_account: &SystemAccount<'info>,
+        lamports: u64,
+    ) -> Result<()> {
+        if !self::is_eligible(fund_account.key) {
+            panic!("Only white listed charity accounts are eligible");
+        }
+
+        let ix = solana_program::system_instruction::transfer(
+            &patron_account.key,
+            &fund_account.key,
+            lamports,
+        );
+
+        solana_program::program::invoke(
+            &ix,
+            &[
+                patron_account.to_account_info(),
+                fund_account.to_account_info(),
+            ],
+        )?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub pixel_wallet: Account<'info, PixelWallet>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct BuyPixels<'info> {
+    #[account(mut)]
+    pub charity: SystemAccount<'info>,
+    #[account(mut)]
+    pub pixel_wallet: Account<'info, PixelWallet>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct PixelWallet {
+    pub available_pixels: u64,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Only white listed charity accounts are allowed")]
+    UnknownCharityAccount,
+}
+
+// #[derive(AnchorSerialize, AnchorDeserialize, Copy, Clone, Default, Debug)]
+// pub struct Point2d {
+//     pub x: u32,
+//     pub y: u32,
+// }
--- tests/ukr_place.ts
+++ tests/ukr_place.ts
@@ -1,16 +1,74 @@
+import { expect } from 'chai';
+import { PublicKey } from "@solana/web3.js";
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { UkrPlace } from "../target/types/ukr_place";
+
+const { SystemProgram } = anchor.web3;
+
+const PRICE_PER_PIXEL = 25_000_000;
+const LAMPORTS_PER_SOL = 1_000_000_000;
+const DONATE_PUBLIC_KEY = new PublicKey("66pJhhESDjdeBBDdkKmxYYd7q6GUggYPWjxpMKNX39KV");
 
 describe("ukr_place", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.UkrPlace as Program<UkrPlace>;
+  const jsonRpc = program.provider.connection;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  const solToLamports = (amount: number) => Math.floor(amount * LAMPORTS_PER_SOL);
+
+  const airdropSol = async (key: PublicKey, sol: number) => {
+    const lamports = solToLamports(sol);
+    const sig = await jsonRpc.requestAirdrop(key, lamports);
+    await jsonRpc.confirmTransaction(sig);
+  };
+
+  // it("Is initialized!", async () => {
+  //   // Add your test here.
+  //   const tx = await program.rpc.initialize({});
+  //   console.log("Your transaction signature", tx);
+  // });
+
+  it("Should buy 144 pixels and transfer money to https://donate.thedigital.gov.ua/", async () => {
+    const pixels = 144;
+    const price = pixels * PRICE_PER_PIXEL;
+    const myMoney = anchor.web3.Keypair.generate();
+    const myPixels = anchor.web3.Keypair.generate();
+
+    await airdropSol(DONATE_PUBLIC_KEY, 3);
+    await airdropSol(myMoney.publicKey, 3.7);
+
+    const accountBefore = await jsonRpc.getAccountInfo(DONATE_PUBLIC_KEY);
+
+    await program.rpc.initialize({
+      accounts: {
+        user: myMoney.publicKey,
+        pixelWallet: myPixels.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [myMoney, myPixels],
+    });
+
+    const sig = await program.rpc.buyPixels(pixels, {
+      accounts: {
+        user: myMoney.publicKey,
+        charity: DONATE_PUBLIC_KEY,
+        pixelWallet: myPixels.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [myMoney],
+    });
+
+    await jsonRpc.confirmTransaction(sig);
+    const accountAfter = await jsonRpc.getAccountInfo(DONATE_PUBLIC_KEY);
+
+    expect(accountAfter.lamports).equal(accountBefore.lamports + price);
+
+    const pixWalletData = await program.account.pixelWallet.fetch(myPixels.publicKey);
+    const availablePixels = pixWalletData.availablePixels.toNumber();
+
+    expect(availablePixels).to.be.equal(pixels);
   });
 });
